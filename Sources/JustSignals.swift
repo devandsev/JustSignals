bcdbd164f474e2e26e0569d8bdd5add294a3d7e4
Andrey Sevrikov <devandsev@gmail.com>
2018-02-23 15:35:50 +0300

    Add Xcode project

---
+++ Sources/JustSignals.swift
@@ -0,0 +1,9 @@
+//
+//  JustSignals.swift
+//  JustSignals
+//
+//  Created by Andrey Sevrikov on 23/02/2018.
+//  Copyright © 2018 Andrey Sevrikov. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import JustSignalsTests
+
+XCTMain([
+    testCase(JustSignalsTests.allTests),
+])
